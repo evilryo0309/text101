7ca22bbc5917ab8bbc00db09f7087f09e29cfb1f
何俊良 <evilryo0309@gmail.com>
2021-04-11 20:56:44 +0800

    Initial Version

---
+++ Assets/Text101/Script/AdvantureGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdvantureGame : MonoBehaviour
+{
+    public Text Story;
+
+    [SerializeField]
+    State state;
+
+    IEnumerator Start()
+    {
+        return ReadStory();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            state = state.nextStorys[0];
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+            state = state.nextStorys[1];
+        else
+            return;
+
+        StartCoroutine(ReadStory());
+    }
+
+    IEnumerator ReadStory()
+    {
+        var text = state.GetStory();
+        for (int i = 0; i < text.Length; i++)
+        {
+            Story.text = text.Substring(0, i);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Text101/Script/State.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "建立故事資料")]
+public class State : ScriptableObject
+{
+    [SerializeField]
+    [TextArea(10, 14)]
+    string story;
+
+    public State[] nextStorys;
+
+    public string GetStory() => story;
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
+}
